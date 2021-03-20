5bc86f7b4cdd07b4dc964607f54b58a08e844b10
Matej <matej.korbar>
2021-03-20 12:58:28 +0100

    Add bucket-list component and routing for it

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BucketListComponent } from './bucket-list/bucket-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buckets', component: BucketListComponent },
+  { path: '**', redirectTo: 'buckets' },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BucketListComponent } from './bucket-list/bucket-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BucketListComponent
   ],
   imports: [
     BrowserModule,
