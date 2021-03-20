5c76da3c8200214affd7d30e5eeb0e5eb7bb3473
Matej <matej.korbar>
2021-03-20 12:29:33 +0100

    Added other routes for /api/buckets

--- backend/routes/buckets.js
+++ backend/routes/buckets.js
@@ -32,7 +32,24 @@ router.get("", (req, res, next) => {
   });
 });
 
-router.get("/:id", (req, res, next) => {
+router.post("", (req, res, next) => {
+  res.status(201).json({
+    message: "Bucket created successfully",
+    bucket: req.body,
+  });
+});
+
+let bucketExists = (req, res, next) => {
+  // TODO: use dynamic
+  if (req.params.id !== "asdf") {
+    return res.status(404).json({
+      message: "bucket not found",
+    });
+  }
+  next();
+};
+
+router.get("/:id", bucketExists, (req, res, next) => {
   res.status(200).json({
     bucket: {
       id: req.params.id,
@@ -44,4 +61,39 @@ router.get("/:id", (req, res, next) => {
     },
   });
 });
+
+router.delete("/:id", bucketExists, (req, res, next) => {
+  res.status(204).send();
+});
+
+router.get("/:id/objects", bucketExists, (req, res, next) => {
+  res.status(200).json({
+    objects: [
+      {
+        name: "first-file.png",
+        modified: "2021-06-24T12:42:50.150Z",
+        size: 1337,
+      },
+      {
+        name: "second-file.png",
+        modified: "2019-06-24T12:42:50.150Z",
+        size: 1234,
+      },
+      {
+        name: "third-file.png",
+        modified: "2018-06-24T12:42:50.150Z",
+        size: 41441,
+      },
+    ],
+  });
+});
+
+router.post("/:id/objects", bucketExists, (req, res, next) => {
+  res.status(201).json({
+    message: "Object created successfully",
+    object: req.body,
+  });
+});
+
+
 module.exports = router;
