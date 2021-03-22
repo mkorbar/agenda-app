52f12586ce56b8c0d551fc72152b38f36946ead5
Matej <matej.korbar>
2021-03-22 15:18:21 +0100

    FE: Implement onBucketDelete and onFileDeltete actions

--- frontend/src/app/bucket-detail/bucket-detail.component.ts
+++ frontend/src/app/bucket-detail/bucket-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { BucketsService } from '../buckets.service';
 import { Bucket } from '../bucket.model';
 import { Object } from '../object.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-bucket-detail',
@@ -19,7 +19,10 @@ export class BucketDetailComponent implements OnInit {
   bucketStorage = 0;
   error = '';
 
-  constructor(private bucketsService: BucketsService, private route: ActivatedRoute) { }
+  constructor(
+    private bucketsService: BucketsService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
     let id = this.route.snapshot.params.id;
@@ -45,6 +48,16 @@ export class BucketDetailComponent implements OnInit {
         this.files.push(data.object);
         this.bucketStorage = this.files.map(obj => obj.size).reduce((a, b) => a + b);
       });
+  }
+
+  onBucketDelete() {
+    this.bucketsService.deleteBucket(this.bucket.id).subscribe(() => {
+      this.router.navigate(['/']);
+    })
+  }
+
+  onFileDelete() {
+    alert("I don't know which file to deltete");
   }
 
 }
